a78102d35bd711e69f1e8e2e9f9119f1c2ed7f74
Yunior González <dev@getinko.com>
2017-05-27 18:53:53 +0100

    trying to ouath twitter

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@
     'use strict';
 
     angular.module('TFG', ['ngRoute'])
-        .config(router);
-
+        .config(router)
+        .constant('TWSecret', 'xsuboKctmcbYGB52h6cpyjO4KhhqoLOee3tkX2m1dY2LcCRnu4')
+        .constant('TWPublic', 'zubZEDS6AG4yrQoDFPZx8ZBjp')
+        .constant('TWToken', '247017729-nGAWynqDVPfnlgJ0HNCVGKWuOQ0dnQWj0tP2TWzt')
+        .constant('TWID', '247017729');
     router.$inject = ['$routeProvider'];
     function router($routeProvider) {
         $routeProvider.when('/', {
