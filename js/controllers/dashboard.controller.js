78c4d5b9b2b389d8cc487d712adec4d87dc8db77
Yunior González <dev@getinko.com>
2017-05-27 16:51:18 +0100

    First iteration on DashBoard Controller and navbar

--- js/controllers/dashboard.controller.js
+++ js/controllers/dashboard.controller.js
@@ -8,14 +8,27 @@
     DashboardController.$inject = [];
     function DashboardController() {
         var vm = this;
-
-
+        vm.search = search;
+        vm.changeSearch = changeSearch;
+        vm.searchedParams = [];
+        vm.actualSearch = '';
         activate();
 
         ////////////////
 
         function activate() {
             vm.title = 'Helo world ! ';
+        }
+
+
+        function search() {
+            vm.searchedParams.push(vm.searchParam);
+            vm.actualSearch = vm.searchParam;
+            vm.searchParam = '';
+        }
+
+        function changeSearch(searchParam) {
+            vm.actualSearch = searchParam;
         }
     }
 })();
