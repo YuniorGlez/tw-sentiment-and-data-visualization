88c7010b453515e6096f5624380582741d1b6067
Yunior González <dev@getinko.com>
2017-05-28 21:04:56 +0100

    Saving the intent to oauth2 with twitter

---
+++ js/services/twitterAuth.service.js
@@ -0,0 +1,23 @@
+
+//  , 'TWSecret', 'TWPublic'
+//  , TWSecret, TWPublic
+        // var consumerKey = encodeURIComponent(TWPublic);
+        // var consumerSecret = encodeURIComponent(TWSecret);
+
+        // var credentials = btoa(consumerKey + ':' + consumerSecret);
+        // console.log(credentials);
+        // // Twitters OAuth service endpoint
+        // $http.post(
+        //     'https://api.twitter.com/oauth2/token',
+        //     // "grant_type=client_credentials",
+        //     {
+        //             'grant_type' : 'client_credentials'
+        //     } ,{
+        //         headers: {
+        //             'Authorization': 'Basic ' + credentials,
+        //             'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        //         }
+        //     })
+        //     .then((response) => console.log(response)
+        //         , (response) => console.log("error ", response)
+        //     )
