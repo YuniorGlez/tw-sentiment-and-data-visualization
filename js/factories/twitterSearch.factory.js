708ed618117bc3f0387632a8dfb2815c604e6a2c
Yunior González <dev@getinko.com>
2017-05-28 21:04:36 +0100

    Factory return fake data

--- js/factories/twitterSearch.factory.js
+++ js/factories/twitterSearch.factory.js
@@ -5,41 +5,16 @@
         .module('TFG')
         .factory('TwitterSearchEngine', TwitterSearchEngine);
 
-    TwitterSearchEngine.$inject = ['$http' , 'TWSecret', 'TWPublic'];
-    function TwitterSearchEngine($http , TWSecret, TWPublic) {
-        var consumerKey = encodeURIComponent(TWPublic);
-        var consumerSecret = encodeURIComponent(TWSecret);
-
-        var credentials = btoa(consumerKey + ':' + consumerSecret);
-        console.log(credentials);
-        // Twitters OAuth service endpoint
-        $http.post(
-            'https://api.twitter.com/oauth2/token',
-            // "grant_type=client_credentials",
-            {
-                    'grant_type' : 'client_credentials'
-            } ,{
-                headers: {
-                    'Authorization': 'Basic ' + credentials,
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-                }
-            })
-            .then((response) => console.log(response)
-                , (response) => console.log("error ", response)
-            )
+    TwitterSearchEngine.$inject = ['$http'];
+    function TwitterSearchEngine($http ) {
 
 
         ////////////////
 
 
         function search(param) {
-            console.log(`Let's go to search ${param} `);
-            return TW.searchTweets(param)
-                .then((data) => {
-                    console.log('Inside factory ', data);
-                    return data
-                })
-                .catch((err) => err);
+            return $http.get('/data/tweetsFakes.json')
+                .then((response) => response.data.map((tweet) => JSON.parse(tweet)));
         }
 
         return {
