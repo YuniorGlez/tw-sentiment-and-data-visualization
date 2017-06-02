1d9d6df829fb6b62d5825c1641cc6e4020c8eaa3
Yunior González <dev@getinko.com>
2017-06-02 09:29:56 +0100

    Adding new type of searchs in factory Twitter

--- js/factories/twitterSearch.factory.js
+++ js/factories/twitterSearch.factory.js
@@ -1,45 +1,61 @@
 (function () {
     'use strict';
-
-    angular
-        .module('TFG')
-        .factory('TwitterSearchEngine', TwitterSearchEngine);
+    angular.module('TFG').factory('TwitterSearchEngine', TwitterSearchEngine);
 
     TwitterSearchEngine.$inject = ['$http', 'TransformService'];
-
     function TwitterSearchEngine($http, T) {
         var api = 'https://tfg-yuniorglez.c9users.io/';
-        var atp = '&access_token_key=';
-        var ats = '&access_token_secret=';
+        var atp = 'access_token_key=';
+        var ats = 'access_token_secret=';
         ////////////////
 
         function search(param) {
             if ('logged' in localStorage && 'tw_object' in localStorage) {
                 var tw = JSON.parse(localStorage.getItem('tw_object'));
                 var url = api + 'search?q=' + encodeURI(param);
-                url += atp + tw.oauthToken + ats + tw.clientSecret;
-                return $http.get(url)
-                    .then(successReadingTweets);
+                url += '&' + atp + tw.oauthToken + '&'+  ats + tw.clientSecret;
+                return $http.get(url).then(successReadingTweets);
             }
-            else {
-                $http.get('/data/anotherFake.json').then(successReadingFakeTweets);
+            else $http.get('/data/anotherFake.json').then(successReadingFakeTweets);
+        }
+
+
+        function searchUserInfo(idUser){
+            if ('logged' in localStorage && 'tw_object' in localStorage) {
+                var tw = JSON.parse(localStorage.getItem('tw_object'));
+                var url = api + 'search/user/' + idUser;
+                url += '?'+ atp + tw.oauthToken + '&' + ats + tw.clientSecret;
+                return $http.get(url).then(successc9Response, (e)=>{console.log(e);return[]});
             }
-            // return $http.get('/data/anotherFake.json').then(successReadingTweets, errorHandler);
+        }
+
+        function searchTweetsFromUser(idUser){
+            if ('logged' in localStorage && 'tw_object' in localStorage) {
+                var tw = JSON.parse(localStorage.getItem('tw_object'));
+                var url = api + 'search/user/tweets/' + idUser;
+                url += '?'+ atp + tw.oauthToken + '&' + ats + tw.clientSecret;
+                return $http.get(url).then(successc9Response, (e)=>{console.log(e);return[]});
+            }
         }
 
         function successReadingTweets(response) {
             var data = response.data;
             var tweetsParsed = response.data;
-            // var tweetsParsed = response.data.map((tw) => JSON.parse(tw));
             console.log('Respuesta del server ', response);
-            return T.transformProcess(tweetsParsed)
-                .then((tweets) => tweets);
+            return T.transformProcess(tweetsParsed).then((tweets) => tweets);
         }
         function successReadingFakeTweets(response) {
             successReadingTweets({ data: response.data.map((tw) => JSON.parse(tw)) });
         }
+
+        function successc9Response(response){
+            console.log('Respuesta del server ', response);
+            return response.data;
+        }
         return {
-            search: search
+            search: search,
+            searchUserInfo : searchUserInfo,
+            searchTweetsFromUser : searchTweetsFromUser
         };
     }
 })();
